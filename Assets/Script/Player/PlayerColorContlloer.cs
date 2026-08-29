24b7bd9f6b90d629ccf98ea32ae5d23993def829
katsura <pk3a4jmn@gmail.com>
2026-08-30 03:40:15 +0900

    PlayerColorContlloerに変更点のメモを追加

--- Assets/Script/Player/PlayerColorContlloer.cs
+++ Assets/Script/Player/PlayerColorContlloer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PlayerColorContlloer : MonoBehaviour
 {
-    
+    //プレイヤー(カメレオン)の色変更用の変数の宣言を追加　桂
     private static readonly Color Black = new Color(0, 0, 0);
     private static readonly Color white = new Color(1, 1, 1);
     private static readonly Color Red = new Color(1,0,0);
@@ -12,9 +12,9 @@ public class PlayerColorContlloer : MonoBehaviour
     private static readonly Color Green = new Color(0, 1, 0);
     private static readonly Color Purple = new Color(1, 0, 1);
     private static readonly Color Orange = new Color(1, 0.5f, 0);
-
     [SerializeField]
     private SpriteRenderer sprite;
+    //
 
     public static PlayerColorContlloer Instance { private set;get;}
     [SerializeField]
@@ -32,9 +32,8 @@ public class PlayerColorContlloer : MonoBehaviour
         //  colorWheelController.SetColor(colorType);
         Debug.Log(color + "に色が変わったよ");
 
-
+        //プレイヤー(カメレオン)の色を更新　桂
         switch (color) {
-
             case ColorType.Black:
                 sprite.color = Black;
                 break;
@@ -59,14 +58,10 @@ public class PlayerColorContlloer : MonoBehaviour
             case ColorType.Orange:
                 sprite.color = Orange;
                 break;
-
-
-
             default:
                 break;
-
         }
-
+        //
 
     }
 }
