650bdbf4c613bda8f9024446f39339f95da48ba2
akiton20100217-pixel <akiton20100217@gmail.com>
2026-08-29 19:03:45 +0900

    19:00_壁を複製して動かすスクリプトをギミックスクリプトフォルダに実装_壁用のプレハブ等は別

---
+++ Assets/Script/Gimmick/ClosingWall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ClosingWall : MonoBehaviour
+{
+    [SerializeField] private GameObject wallPrefub;
+    [SerializeField] private GameObject instanPoint;
+
+    [SerializeField] private float closingSpeed = 5f;
+    [SerializeField] private float closingDistance = 10f;
+
+    private void Start()
+    {
+        StartCoroutine(CloseWall());
+    }
+
+    IEnumerator CloseWall()
+    {
+        while (true)
+        {
+            InstanWall();
+            //Debug.Log("壁が閉じるよ");
+
+            yield return new WaitForSeconds(closingDistance);
+        }
+    }
+
+    void InstanWall()
+    {
+        GameObject wall = Instantiate(wallPrefub, instanPoint.transform.position, Quaternion.identity);
+
+        Rigidbody2D rb = wall.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = new Vector2(closingSpeed, 0);
+        //wall.transform.position += new Vector3(0, closingSpeed * Time.deltaTime, 0);
+
+    }
+}
