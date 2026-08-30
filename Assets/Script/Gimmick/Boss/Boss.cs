ee7be67cf683c4f9378bd839ab0ccff1b85888d1
akiton20100217-pixel <akiton20100217@gmail.com>
2026-08-30 11:27:34 +0900

    08/30_ボスのギミックスクリプト_ポイントを通過すると壁を生成し一定時間後に削除・プレイヤーに触れるとレイヤーの判定を行い同じだと何もなく、違うとゲームオーバーのログを送る(処理は後で追加)_ボス関係のギミックをscriptsフォルダのBossフォルダにまとめました

---
+++ Assets/Script/Gimmick/Boss/Boss.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private void Start()
+    {
+        BossAwake();
+    }
+
+    private void BossAwake()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            // プレイヤーがボスに当たったときの処理
+            Debug.Log("プレイヤーがボスに当たった！");
+        }
+    }
+}
---
+++ Assets/Script/Gimmick/Boss/BossWall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BossWall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            if (collider.gameObject.layer == gameObject.layer)
+            {
+                return;
+            }
+            else
+            {
+                //ゲームオーバー処理
+                Debug.Log("ゲームオーバー");
+            }
+        }
+    }
+}
